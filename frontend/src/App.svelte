dfb42fdccdf3da955996ed1c0e799b25b740b456
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2022-08-10 12:41:10 +0600

    lol

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -7,17 +7,8 @@ import Aboutus from "./routes/aboutus.svelte";
 import Accinfo from "./routes/accinfo.svelte";
 import Logout from "./routes/logout.svelte";
 import Basesearchresults from "./routes/basesearchresults.svelte";
+import Animesearchresult from "./routes/animesearchresult.svelte";
 
-
-
-let searchtype='';
-let searchtxt='';
-$:{
-    search.set({
-        txt:searchtype,
-        type:searchtype
-    })
-}
 </script>
 <Router>
 <div class="flex flex-col w-64 h-screen px-4 py-8 bg-white border-r dark:bg-gray-800 dark:border-gray-600">
@@ -30,7 +21,7 @@ $:{
             </svg>
         </span>
 
-        <input type="text" bind:value={searchtxt} class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring" placeholder="Search"/>
+        <input type="text" bind:value={$search.txt} class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring" placeholder="Search"/>
     </div>
     <div class="relative mt-6">
         <span class="absolute inset-y-0 left-0 flex items-center pl-3">
@@ -39,9 +30,8 @@ $:{
             </svg>
         </span>
 
-        <input type="text" bind:value={searchtype} class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring" placeholder="Search Type"/>
+        <input type="text" bind:value={$search.type} class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring" placeholder="Search Type"/>
     </div>
-    
     <div class="flex flex-col justify-between flex-1 mt-6">
         <nav>
             <a class="flex items-center px-4 py-2 text-gray-700 bg-gray-200 rounded-md dark:bg-gray-700 dark:text-gray-200" href="searchresult">
@@ -122,7 +112,11 @@ $:{
         <Signup/>
     </Route>
     <Route path="/searchresult">
+        {#if $search.type == "anime" }
+        <Animesearchresult/>
+         {:else}
         <Basesearchresults/>
+        {/if}
     </Route>
     <Route path="/aboutus">
         <Aboutus/>
