8d06852f540cace91382689632bb902632143726
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2022-08-11 19:14:46 +0600

    trying something new

---
+++ new_frontend/testfrontend/src/stores/store.js
@@ -0,0 +1,16 @@
+import { writable } from 'svelte-local-storage-store';
+export let curruser=writable('crruser',
+{
+    name:'',
+    mail:''
+})
+export let searchresult = writable('searchresult',{
+    success: false,
+    results: [],
+  });
+export let search=writable('search',{
+    txt:'',
+    type:''
+}
+)
+export let state=writable('state',0);
