5bede20e1aff63405a8e9029da446207131ce4c9
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2022-08-12 15:59:49 +0600

    Index page made

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -8,6 +8,7 @@ export let searchresult = writable('searchresult',{
     success: false,
     results: [],
   });
+export let picturelinks= writable('picturelinks',[]);
 export let search=writable('search',{
     txt:'',
     type:''
