fd92d54425ad8cab9602c3928e6f9756027451c3
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2022-08-22 01:55:44 +0600

    Handled Random anime showcase

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -7,6 +7,24 @@
    let animes;
  let endpoint="http://localhost:5000/anime";
  let image = "http://localhost:5000/uploads/images/";
+ function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
  async function proxyfetchanimeid()
  {
    const response = await fetch(endpoint);
@@ -58,6 +76,7 @@
 			}
 		}
 		//console.log(resanimes);
+		shuffle(resanimes);
 		$allanimes = resanimes;
 		//console.log($allanimes);
 	}
