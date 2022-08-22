b2d67f6db69ec39be22859ce87194fd1902c825a
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2022-08-22 21:43:20 +0600

    cosmetic kaj kori chipa diya

--- frontend/src/components/altnav.svelte
+++ frontend/src/components/altnav.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { state, curruser, search,searchdest } from './../stores/store.js';
+	import { state, curruser, search, searchdest } from './../stores/store.js';
 	import {
 		Navbar,
 		NavBrand,
@@ -14,34 +14,25 @@
 		Radio,
 		Input,
 		Button,
+SimpleSearch
 	} from 'flowbite-svelte';
 	import { ArrowRight } from 'svelte-heros';
-	$:
-	{
-	//	console.log($search.txt);
-	//	console.log($search.type);
-	//	console.log($curruser);
-		if($search.type=='anime')
-	
-  {
-    $searchdest="/animesearchresult";
-  }
-  else if($search.type=='user')
-  {
-    $searchdest="/usersearch";
-  }
-  else if($search.type=='character')
-  {
-    $searchdest="/charactersearch";
-  }
-  else if($search.type=='studio')
-  {
-    $searchdest="/studiosearch";
-  }
-  else if($search.type=='personnel')
-  {
-    $searchdest="/personnelsearch";
-  }
+	import { SearchCircle } from 'svelte-heros';
+	$: {
+		//	console.log($search.txt);
+		//	console.log($search.type);
+		//	console.log($curruser);
+		if ($search.type == 'anime') {
+			$searchdest = '/animesearchresult';
+		} else if ($search.type == 'user') {
+			$searchdest = '/usersearch';
+		} else if ($search.type == 'character') {
+			$searchdest = '/charactersearch';
+		} else if ($search.type == 'studio') {
+			$searchdest = '/studiosearch';
+		} else if ($search.type == 'personnel') {
+			$searchdest = '/personnelsearch';
+		}
 	}
 </script>
 
@@ -60,38 +51,37 @@
 		<div class="hidden relative md:block">
 			<Input bind:value={$search.txt} id="search-navbar" class="pl-10" placeholder="Search..." />
 		</div>
-		<Dropdown label="Search Criteria" class="w-48">
+		<Dropdown label={$search.type} class="w-48">
 			<ul slot="content" class="p-3 space-y-1">
-			  <DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-				<Radio bind:group={$search.type} value={"anime"}>Anime</Radio>
-			  </DropdownItem>
-			  <DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-				<Radio bind:group={$search.type} value={"character"}>Character</Radio>
-			  </DropdownItem>
-			  <DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-				<Radio bind:group={$search.type} value={"user"}>User</Radio>
-			  </DropdownItem>
-			  <DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-				<Radio bind:group={$search.type} value={"studio"}>Studio</Radio>
-			  </DropdownItem>
-			  <DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-				<Radio bind:group={$search.type} value={"personnel"}>Personnel</Radio>
-			  </DropdownItem>
+				<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+					<Radio bind:group={$search.type} value={'anime'}>Anime</Radio>
+				</DropdownItem>
+				<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+					<Radio bind:group={$search.type} value={'character'}>Character</Radio>
+				</DropdownItem>
+				<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+					<Radio bind:group={$search.type} value={'user'}>User</Radio>
+				</DropdownItem>
+				<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+					<Radio bind:group={$search.type} value={'studio'}>Studio</Radio>
+				</DropdownItem>
+				<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+					<Radio bind:group={$search.type} value={'personnel'}>Personnel</Radio>
+				</DropdownItem>
 			</ul>
-		  </Dropdown>
-		  <a href={$searchdest}>
-		  <Button  pill="{true}" class="!p-2" outline="{true}"><ArrowRight class="w-5 h-5" />Go...</Button>
+		</Dropdown>
+		<a href={$searchdest}>
+			<SearchCircle class="w-10 h-10" />
 		</a>
-		  <NavHamburger on:click={toggle} />
+		<NavHamburger on:click={toggle} />
 	</div>
 	<NavUl {hidden}>
 		<NavLi href="/" active={true}>Home</NavLi>
 		<NavLi href="/ratedanime">Top Rated</NavLi>
 		<NavLi href="/aboutus">About</NavLi>
-		
 	</NavUl>
-	<div class="flex md:order-2">
-		<Dropdown arrowIcon={false} inline={true}>
+	<div class="flex md:order-2" >
+		<Dropdown arrowIcon={false} inline={true} >
 			<Avatar src={$curruser.image} slot="label" />
 			{#if $state == 1}
 				<DropdownHeader>
@@ -99,12 +89,12 @@
 					<span class="block truncate text-sm font-medium"> {$curruser.mail} </span>
 				</DropdownHeader>
 				<a href="/watchlist">
-				<DropdownItem>Watchlist</DropdownItem>
-			</a>
+					<DropdownItem>Watchlist</DropdownItem>
+				</a>
 				<DropdownItem>WatchRoom</DropdownItem>
 				<a href="/accinfo">
-				<DropdownItem>More Info</DropdownItem>
-			</a>
+					<DropdownItem>More Info</DropdownItem>
+				</a>
 				<DropdownDivider />
 				<a href="/logout">
 					<DropdownItem>Log out</DropdownItem>
