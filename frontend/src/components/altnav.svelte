29c33f9c05ec4be4a9652e0ac077c2dffdcd5ac7
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2022-08-30 21:35:17 +0600

    home lagbo na jaiga kheye dicce

--- frontend/src/components/altnav.svelte
+++ frontend/src/components/altnav.svelte
@@ -16,7 +16,7 @@
 		Iconinput,
 		Alert
 	} from 'flowbite-svelte';
-	let logchoice=0;
+	let logchoice = 0;
 	function handlelogout() {
 		state.set(0);
 		curruser.set({
@@ -24,7 +24,7 @@
 			mail: '',
 			image: '',
 			token: '',
-			admin:false
+			admin: false
 		});
 		search.set({
 			txt: '',
@@ -32,16 +32,13 @@
 		});
 		resetalert();
 	}
-	function logout()
-	{
-		logchoice=1;
+	function logout() {
+		logchoice = 1;
 	}
-	function resetalert()
-	{
-		logchoice=0;
+	function resetalert() {
+		logchoice = 0;
 	}
-	
-	
+
 	$: {
 		//	console.log($search.txt);
 		//	console.log($search.type);
@@ -59,15 +56,18 @@
 		}
 	}
 </script>
-{#if logchoice==1}
-<Alert>
-	<div class="flex justify-center"></div>
-	<span class="text-lg font-medium text-red-700 dark:text-blue-800">Do You Really Want To Log Out?</span>
-	<Button on:click={handlelogout} size="xs" outline color="blue">Yes</Button>
-	<Button on:click={resetalert} size="xs" outline color="blue">No</Button>
-</Alert>
+
+{#if logchoice == 1}
+	<Alert>
+		<div class="flex justify-center" />
+		<span class="text-lg font-medium text-red-700 dark:text-blue-800"
+			>Do You Really Want To Log Out?</span
+		>
+		<Button on:click={handlelogout} size="xs" outline color="blue">Yes</Button>
+		<Button on:click={resetalert} size="xs" outline color="blue">No</Button>
+	</Alert>
 {/if}
-<Navbar let:hidden let:toggle rounded={true} >
+<Navbar let:hidden let:toggle rounded={true} class="flex justify-center">
 	<NavBrand href="/">
 		<img
 			src="http://localhost:5000/uploads/images/anidex.jpg"
@@ -79,60 +79,62 @@
 		</span>
 	</NavBrand>
 	<NavUl>
-	  <Dropdown label={$search.type} class="w-48">
-		<ul slot="content" class="p-3 space-y-1">
-			<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-				<Radio bind:group={$search.type} value={'anime'}>Anime</Radio>
-			</DropdownItem>
-			<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-				<Radio bind:group={$search.type} value={'character'}>Character</Radio>
-			</DropdownItem>
-			<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-				<Radio bind:group={$search.type} value={'user'}>User</Radio>
-			</DropdownItem>
-			<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-				<Radio bind:group={$search.type} value={'studio'}>Studio</Radio>
-			</DropdownItem>
-			<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-				<Radio bind:group={$search.type} value={'personnel'}>Personnel</Radio>
-			</DropdownItem>
-		</ul>
-	</Dropdown>
-	<form>
-		<div class="flex relative">
-		<Iconinput  bind:value={$search.txt} noBorder id="search"  placeholder="Search Text"  />
-		<p> &nbsp</p>
-		<a href={$searchdest}>
-		<Button textSize="text-sm" class="text-white absolute right-5.5 bottom-0 flex md:order-1" type="submit">Search</Button>
-	</a>
-		</div>
-	  </form>
+		<Dropdown label={$search.type} class="w-48">
+			<ul slot="content" class="p-3 space-y-1">
+				<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+					<Radio bind:group={$search.type} value={'anime'}>Anime</Radio>
+				</DropdownItem>
+				<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+					<Radio bind:group={$search.type} value={'character'}>Character</Radio>
+				</DropdownItem>
+				<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+					<Radio bind:group={$search.type} value={'user'}>User</Radio>
+				</DropdownItem>
+				<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+					<Radio bind:group={$search.type} value={'studio'}>Studio</Radio>
+				</DropdownItem>
+				<DropdownItem class="rounded" liClass="p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+					<Radio bind:group={$search.type} value={'personnel'}>Personnel</Radio>
+				</DropdownItem>
+			</ul>
+		</Dropdown>
+		<form>
+			<div class="flex relative">
+				<Iconinput bind:value={$search.txt} noBorder id="search" placeholder="Search Text" />
+				<p>&nbsp</p>
+				<a href={$searchdest}>
+					<Button
+						textSize="text-sm"
+						class="text-white absolute right-5.5 bottom-0 flex md:order-1"
+						type="submit">Search</Button
+					>
+				</a>
+			</div>
+		</form>
 	</NavUl>
-	<NavUl {hidden}>
-		<NavLi href="/" >Home</NavLi>
+	<NavUl {hidden} >
 		<Dropdown label="Database" inline={true}>
 			<a href="allanime">
-			<DropdownItem>Anime</DropdownItem>
-		</a>
-		<a href="allcharacters">
-			<DropdownItem>Character</DropdownItem>
-		</a>
+				<DropdownItem>Anime</DropdownItem>
+			</a>
+			<a href="allcharacters">
+				<DropdownItem>Character</DropdownItem>
+			</a>
 			<DropdownItem>Personnel</DropdownItem>
 		</Dropdown>
-		
+
 		<NavLi href="/ratedanime">Top Rated</NavLi>
 		<NavLi href="/aboutus">About</NavLi>
 	</NavUl>
 	<div class="flex  justify-end z-50">
 		<Dropdown arrowIcon={false} inline={true} class="bg-white overflow-visible">
-			
-			<Avatar src={$curruser.image} slot="label"  />
+			<Avatar src={$curruser.image} slot="label" />
 			{#if $state == 1}
 				<DropdownHeader>
 					<span class="block text-sm"> {$curruser.name} </span>
 					<span class="block truncate text-sm font-medium"> {$curruser.mail} </span>
-					{#if $curruser.admin==true}
-					<span class="block truncate text-sm font-medium"> Admin </span>
+					{#if $curruser.admin == true}
+						<span class="block truncate text-sm font-medium"> Admin </span>
 					{/if}
 				</DropdownHeader>
 				<a href="/accinfo">
@@ -144,44 +146,33 @@
 				<a href="/favourites">
 					<DropdownItem>Favourites</DropdownItem>
 				</a>
-					<DropdownItem>
-						<Dropdown label="Add" inline={true} placement="left-start" class="ml-16 w-44">
-							
-							<a href="/addanime">
-							<DropdownItem>
-								Add Anime
-							</DropdownItem>
+				<DropdownItem>
+					<Dropdown label="Add" inline={true} placement="left-start" class="ml-16 w-44">
+						<a href="/addanime">
+							<DropdownItem>Add Anime</DropdownItem>
 						</a>
 						<a href="/addpersonnel">
-							<DropdownItem>
-								Add Personnel
-							</DropdownItem>
+							<DropdownItem>Add Personnel</DropdownItem>
 						</a>
 						<a href="/addcharacter">
-							<DropdownItem>
-								Add Character
-							</DropdownItem>
+							<DropdownItem>Add Character</DropdownItem>
 						</a>
 						<a href="/addstudio">
-							<DropdownItem>
-								Add Studio
-							</DropdownItem>
+							<DropdownItem>Add Studio</DropdownItem>
 						</a>
 						<a href="/addgenre">
-							<DropdownItem>
-								Add Genre
-							</DropdownItem>
+							<DropdownItem>Add Genre</DropdownItem>
 						</a>
-						</Dropdown>
-					</DropdownItem>
-					{#if $curruser.admin==true}
+					</Dropdown>
+				</DropdownItem>
+				{#if $curruser.admin == true}
 					<a href="/admin">
-				<DropdownItem>Admin Page</DropdownItem>
-			</a>
-			{/if}
+						<DropdownItem>Admin Page</DropdownItem>
+					</a>
+				{/if}
 				<DropdownDivider />
-				
-					<DropdownItem on:click={logout}>Log out</DropdownItem>
+
+				<DropdownItem on:click={logout}>Log out</DropdownItem>
 			{:else}
 				<a href="/login">
 					<DropdownItem>Log in</DropdownItem>
